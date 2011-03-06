f80026cb78648c595c7402735f0fba6627d89941
digdog <digdog@mac.com>
2011-03-06 13:21:28 -0800

    Initial Commit

---
+++ UIDickBar/UIDickBarAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  UIDickBarAppDelegate.h
+//  UIDickBar
+//
+//  Created by digdog on 3/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDickBarAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
